4501de938df25009b8556a01d3c638a0789a576d
Dan Nguyen <Dan@Dans-MacBook-Pro.local>
2015-07-29 20:44:12 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,27 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    
+    timeSinceObstacle = 0.0f;
+   
+    
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 -(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
